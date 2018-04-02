baa2d9567217446c45862b258f54bda06326c04b
nhatnv <nguyenvannhat152@gmail.com>
2018-04-02 11:43:16 +0700

    remove data

--- lifelong.py
+++ lifelong.py
@@ -10,23 +10,23 @@ def save_prev_words(prev_words):
 def word_previous_entity(path):
     print(path)
     sentences = utils.parse(utils.read(path), ["\n\n", "\n", "\t"])
-    output = {}
+    output = {k: set() for k in ["ORG", "LOC", "PER", "MIC"]}
     for s in sentences:
         for w, prev_w in zip(s[1:], s[:-1]):
             if w[-2] != "O" and prev_w[-2] == 'O' and prev_w[0] not in utils.punctuations:
                 tag = w[-2].split("-")[-1]
-                if tag not in output:
-                    output[tag] = set()
                 output[tag].add(prev_w[0])
     return output
 
 
-def main():
-    words = word_previous_entity("data/ner/-Doi_song_train.muc")
-    save_prev_words(words)
-
-
 def update(prev_words, labeled_words, min_count=0):
+    """
+
+    :param prev_words: prev_words.json hiện tại
+    :param labeled_words: [(word1, ner_tag1), (word2, ner_tag2), ...]
+    :param min_count:
+    :return: prev_words mới
+    """
     counters = {}
     for word, tag, prev_word, prev_tag in zip(labeled_words[1:], labeled_words[:-1]):
         if tag != 'O' and prev_tag != 'O' and prev_word not in utils.punctuations:
@@ -39,10 +39,14 @@ def update(prev_words, labeled_words, min_count=0):
     for tag, words_count in counters.items():
         for word, count in words_count.items():
             if count > min_count:
-                if tag not in prev_words:
-                    prev_words[tag] = set()
-                prev_words[tag].add(word)
+                prev_words[tag].append(word)
+    prev_words = {k: list(set(v)) for k, v in prev_words.items()}
     return prev_words
+
+
+def main():
+    words = word_previous_entity("data/ner/-Doi_song_train.muc")
+    save_prev_words(words)
 
 
 if __name__ == "__main__":
